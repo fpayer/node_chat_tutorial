9b27e7d107f3e1e8e8081b4f09307b1527c8a19d
fpayer <fpayer@tacnetsol.com>
2015-04-08 22:19:33 -0400

    Completed Application

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* Dependencies */
 var express = require('express');
 var http = require('http');
-var busyboy = require('connect-busboy');
+var busboy = require('connect-busboy');
 var fs = require('fs');
 
 /* Configuration */
@@ -10,14 +10,35 @@ var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 /* Server Resources */
 var app = express();
 var server = http.createServer(app);
+var users = [ ];
+
+/* Helper functions */
+var remove = function(arr, item) {
+  var index = arr.indexOf(item);
+  arr.splice(index, 1);
+}
 
 /* Socket.io connections */
 var io = require('socket.io')(server);
 io.on('connection', function(client) {
   console.log('Client connected from', client.handshake.address);
 
+  client.on('message', function(message) {
+    io.sockets.emit('message', message);
+  });
+
+  client.on('name', function(name) {
+    client.name = name;
+    users.push(name);
+    io.sockets.emit('users', users);
+  });
+
   client.on('disconnect', function() {
     console.log('Client', client.handshake.address, 'disconnected');
+    if (client.name) {
+      remove(users, client.name);
+      client.broadcast.emit('users', users);
+    }
   });
 });
 
@@ -26,7 +47,31 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.use(busboy({
+  limits : {
+    fileSize : config.max_filesize * 1024 * 1024
+  }
+}));
+
+app.post('/upload', function(req, res) {
+  req.busboy.on('file', function(fieldname, file, filename, encoding, mimetype) { 
+    var path = __dirname + '/uploads/';
+    var fstream = fs.createWriteStream(path + filename);
+    file.pipe(fstream);
+    fstream.on('close', function() {
+      res.redirect('back');
+      io.sockets.emit('upload', {
+        url : '/uploads/' + filename,
+        filename : filename
+      });
+    });
+  });
+  req.pipe(req.busboy);
+});
+
 app.use('/static/', express.static(__dirname + '/static/'));
+app.use('/uploads/', express.static(__dirname + '/uploads/'));
+
 
 /* Server startup */
 server.listen(config.port);
--- static/js/utils.js
+++ static/js/utils.js
@@ -15,8 +15,12 @@ toastr.options = {
   showMethod: 'fadeIn',
   hideMethod: 'fadeOut'
 };
+var name = '';
 
-/* Helper function */
+/* Helper functions */
+/*
+ * Creates a positive or negative toast notification depending on the status provided
+*/
 var notify = function(success, error, stat) {
   if (stat === null) {
     toastr.success(success);
@@ -29,49 +33,186 @@ var notify = function(success, error, stat) {
   }
 };
 
+/*
+ * Generates a random number between the interval (inclusive)
+*/
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var utils = new (function() {
+  var get_funcs = {
+    /* 
+     * Grabs the name provided by the name modal
+    */
+    name : function() {
+      var name = $('#name-input').val();
+      if (name === '') {
+        name = 'Guest' + getRandomInt(10, 99);
+      }
+      return name;
+    },
+    /*
+     * Grabs the message the user inputed
+    */
+    message : function() {
+      return $('#message-input').val();
+    }
+  };
+
+  var populate_funcs = {
+    /*
+     * Populates user list
+    */
+    users : function(data) {
+      //Remove old user list
+      $('#user-list').empty();
+
+      //Add new users
+      var x = 0;
+      while (x < data.length) {
+        $('#user-list').append(
+          $('<div>').addClass('item').text(data[x])
+        );
+        x++;
+      }
+    },
+    /*
+     * Adds message to chat body
+    */
+    message : function(data) {
+     //Add message to body
+      $('#messages').append(
+        $('<tr>').append(
+          $('<td>').text(data.name + ': ' + data.message)
+        )
+      );
+      //Scroll chat window
+      $("#chatter").animate({ scrollTop: $('#chatter').prop('scrollHeight')}, 50);     
+    },
+    /*
+     * Adds file download link to chat body
+    */
+    upload : function(data) {
+      $('#messages')
+        .append($('<tr>')
+          .append($('<td>')
+            .append($('<div>')
+              .append($('<span>')
+                .text('A user has uploaded ')
+              )
+              .append($('<a>')
+                .text(data.filename)
+                .attr('href', data.url)
+                .attr('target', '_blank')
+              )
+            )
+          )
+        )
+      ;
+      //Scroll chat window
+      $("#chatter").animate({ scrollTop: $('#chatter').prop('scrollHeight')}, 50);     
+    }
+  };
+
+  var send_funcs = {
+    /*
+     * Gets user-provided name and passes it to the server
+    */
+    name : function() {
+      name = get_funcs.name();
+      socket.emit('name', name);
+      $('#name-modal').modal('hide');
+    },
+    /*
+     * Gets user-provided message and passes it to the server
+    */
+    message : function() {
+      var input = get_funcs.message();
+      if (input !== '') {
+        //Send data to server
+        socket.emit('message', {
+          name : name,
+          message : input
+        });
+        //Clear out old message
+        $('#message-input').val('');
+      }
+    }
+  };
+
+  return {
+    get : function(val) {
+      return get_funcs[val]();
+    },
+    populate : function(val, data) {
+      populate_funcs[val](data);
+    },
+    send : function(val) {
+      send_funcs[val]();
+    }
+  }
+});
+  
+/* Socket Configuration */
+socket.on('message', function(data) {
+  utils.populate('message',data); 
+});
+
+socket.on('users', function(data) {
+  utils.populate('users', data);
+});
+
+socket.on('upload', function(data) {
+  utils.populate('upload', data);
+});
+
+/* UI Configuration */
 $(document).ready(function() {
-  /* UI Configuration */
-  $('#whobox')
+  //Create popup to ask for name
+  $('#name-modal')
     .modal({
-      closable : false
+      closable : false,
+      onShow : function() {
+        $('#name-input').focus();
+      }
     })
     .modal('show')
   ;
 
-  /*
-  setInterval(function() {
-    $('#chatter').scrollTop($('#chatter').scrollTop() + 10);
-  }, 10);
-  */
+  //Submit name on enter
+  $('#name-input').on('keyup', function(e) {
+    if (e.keyCode === 13) {
+      utils.send('name');
+    }
+  });
   
-  $('#name_btn').on('click', function(){
-    name = $('#username').val();
-    $('#whobox').modal('hide');
-    return false;
+  //Submit name on button press
+  $('#name-btn').on('click', function() {
+    utils.send('name'); 
   });
 
-  $('#send_btn').on('click', function(){
-    var msgStr = $('#m').val();
-    socket.emit('chat_msg', { name: name, msg: msgStr });
-    console.log('Message sent: ' + msgStr);	
-    $('#m').val('');
-    return false;
+  //Submit message on enter
+  $('#message-input').on('keyup', function(e) {
+    if (e.keyCode === 13) {
+      utils.send('message');
+    }
+  });
+  
+  //Submit message on button press
+  $('#message-btn').on('click', function() {
+    utils.send('message'); 
   });
 
+  //Handles file uploads
   var uploader = new Dropzone('#dropzone', {
     url : '/upload',
     maxFilesize : 25
-  });
-
-  uploader.on('complete', function(file) {
+  }).on('complete', function(file) {
     uploader.removeFile(file);
-  });
-
-  uploader.on('error', function(file) {
+  }).on('error', function(file) {
     notify('', 'Failed to upload ' + file.name, '. It may be over 25MB');
-  });
-
-  uploader.on('success', function(file) {
+  }).on('success', function(file) {
     notify('Successfully uploaded ' + file.name, '', null);
   });
 });
